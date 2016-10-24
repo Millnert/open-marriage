dc22b13e7ec31b012f35ee77309b440a09c448a8
Martin Millnert <martin@millnert.se>
2016-10-25 00:09:31 +0200

    config/date: update to dissertation

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ config = {
         emails  : path.resolve('views/emails/')
     },
 
-    date: new Date('Sat Oct 5 2013 4:30:00 GMT-0400 (EDT)'),
+    date: new Date('Mon Dec 19 2016 12:30:00 GMT+0200 (CET)'),
 
     version: require('../package').version,
 
