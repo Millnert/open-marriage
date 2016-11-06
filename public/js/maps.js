860415d446db1130a31e7966de4a2ecf352dfc2d
Martin Millnert <martin@millnert.se>
2016-11-06 20:57:47 +0100

    maps: use own style

--- public/js/maps.js
+++ public/js/maps.js
@@ -10,7 +10,7 @@ YUI.add('le-maps', function (Y) {
 	mapboxgl.accessToken = 'pk.eyJ1IjoibWlsbG5lcnQiLCJhIjoiY2l2NnUxNjF2MDAwMTJ4bXZqNmJ4eHZuMiJ9.8CvG2KOAWS3dzDzPFBDNkg';
         var map = new mapboxgl.Map({
             container: 'map', // container id
-            style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
+            style: 'mapbox://styles/millnert/civ6u34b500572ilc703iaagy', //stylesheet location
             center: [13.208, 55.710], // starting position
             zoom: 18 // starting zoom
 });
