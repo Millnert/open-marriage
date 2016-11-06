d68dbb4f00fc2c24671754201eec41bcec5476a1
Martin Millnert <martin@millnert.se>
2016-11-06 18:07:59 +0100

    maps: no fraction in zoom level

--- public/js/maps.js
+++ public/js/maps.js
@@ -4,7 +4,7 @@ YUI.add('le-maps', function (Y) {
 
     Y.all('[data-map]').each(function (mapNode) {
         L.mapbox.accessToken = 'pk.eyJ1IjoibWlsbG5lcnQiLCJhIjoiY2l2NnUxNjF2MDAwMTJ4bXZqNmJ4eHZuMiJ9.8CvG2KOAWS3dzDzPFBDNkg';
-        var map = L.mapbox.map('map', 'mapbox.streets').setView([55.710, 13.208], 17.4);
+        var map = L.mapbox.map('map', 'mapbox.streets').setView([55.710, 13.208], 18);
     });
 
 }, '1.8.0', {
